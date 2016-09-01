4b51ac955aeb242cb1118c063d67dbd0fa05ac7e
Daniel Hoffens <dhoffens@gmail.com>
2016-09-01 14:13:54 -0400

    js setTimeout

--- Week3/day4/callbacks/app.js
+++ Week3/day4/callbacks/app.js
@@ -1,45 +1,69 @@
 "use strict";
 
-var fs = require("fs");
+// var movieFileContents = File.read("movies.txt");
 
-fs.readFile("movies.txt", "utf8", function (theError, movieFileContents) {
-	if (theError === null) {
-	var moviesArray = movieFileContents.split("\n");
-	console.log(moviesArray);
-	}
-	else {
-		console.log("Oh no! Error.")
-		console.log( theError )
-	}
-});
+// var fs = require("fs");
+
+// fs.readFile("movies.txt", "utf8", function (theError, movieFileContents) {
+// 	if (theError === null) {
+// 	var moviesArray = movieFileContents.split("\n");
+// 	console.log(moviesArray);
+// 	}
+// 	else {
+// 		console.log("Oh no! Error.")
+// 		console.log( theError )
+// 	}
+// });
 
 var read = require("read");
 
-read
+var theOptions = { prompt: "What do you want?"};
 
-// var movieFileContents = File.read("movies.txt");
+function greetUser (theError, givenName) {
+	console.log(`Hello,  ${givenName}!`);
+}
+
+read(theOptions, greetUser);
+
+
 
 // var moviesArray = movieFileContents.split("\n");
-// console.log(moviesArray);
+// 	console.log(moviesArray);
 
-// function after5Seconds () {
-// 	console.log("5 seconds");
-// }
+function after5Seconds () {
+	console.log("Loading....");
+}
 
-// setTimeout(after5Seconds, 5000);
+setTimeout(after5Seconds, 1000);
 
-// function after10Seconds () {
-// 	console.log("10 seconds");
-// }
+setTimeout(after5Seconds, 1500);
+setTimeout(after5Seconds, 2000);
+setTimeout(after5Seconds, 2500);
+setTimeout(after5Seconds, 3000);
+setTimeout(after5Seconds, 3500);
+setTimeout(after5Seconds, 4000);
+setTimeout(after5Seconds, 4100);
+setTimeout(after5Seconds, 4300);
+setTimeout(after5Seconds, 4500);
+setTimeout(after5Seconds, 4600);
+setTimeout(after5Seconds, 4700);
+setTimeout(after5Seconds, 4800);
+setTimeout(after5Seconds, 4900);
+setTimeout(after5Seconds, 5000);
 
-// setTimeout(after10Seconds, 10000);
 
-// function after1Second () {
-// 	console.log("1 seconds");
-// }
+function after10Seconds () {
+	console.log("10 seconds");
+}
 
-// setTimeout(after1Second, 1000);
+setTimeout(after10Seconds, 10000);
 
-// console.log("");
-// console.log("END OF PROGRAM");
-// console.log("");
+function after1Second () {
+	console.log("1 seconds");
+}
+
+setTimeout(after1Second, 1000);
+
+console.log("");
+console.log("END OF PROGRAM");
+console.log("");
