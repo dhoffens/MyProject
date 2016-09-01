73f97276e538eae8f44d87f79feca05ea9324bf3
Daniel Hoffens <dhoffens@gmail.com>
2016-09-01 10:15:14 -0400

    js syntax iteration 2 with classes

--- Week3/day4/ruby-js-review/app.js
+++ Week3/day4/ruby-js-review/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var Animal = require("./lib/animal.js")
 
 var dymitri = new Animal("dymitri", "Huff...");
@@ -7,11 +9,4 @@ dymitri.shout();
 var chewie = new Animal("chewie", "woof");
 chewie.makeNoise();
 chewie.shout();
-
-dymitri.makeNoise();
-dymitri.shout();
-
-chewie.makeNoise();
-chewie.shout();
-
 
--- Week3/day4/ruby-js-review/lib/animal.js
+++ Week3/day4/ruby-js-review/lib/animal.js
@@ -1,16 +1,16 @@
-function Animal (name, noise) {
-	this.name = name;
+"use strict";
 
+class Animal {
+
+	constructor (name, noise) {
+	this.name = name;
 	this.noise = noise;
 }
-
-Animal.prototype.makeNoise = function () {
-		console.log(this.name + " says: " + this.noise );
+	makeNoise () {
+		console.log( `${this.name} says: ${this.noise}` );
 	};
-
-
-Animal.prototype.shout = function () {
+	shout () {
 		console.log(this.noise.toUpperCase() );
 	};
-
+}
 module.exports = Animal;
