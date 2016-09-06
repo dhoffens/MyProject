c497ea86bf1ea9931bedbb589ce27d2bbed82808
Daniel Hoffens <dhoffens@gmail.com>
2016-09-06 17:34:45 -0400

    js quiz with loop to go over questions

--- Week4/js-personality/app.js
+++ Week4/js-personality/app.js
@@ -2,40 +2,68 @@
 
 var read = require('read');
 
-var options = {
-	prompt: "Do you find it difficult to introduce yourself to others?"
-};
+var questionStrings = [
+	"Do you find it difficult to introduce yourself to others?",
+	"Do you find yourself initiating conversations?",
+	"Do you feel superior to others?",
+	"Do you clean up after yourself?",
+	"Do you enjoy being the center of attention?",
+	"Do you pick up a piece of food and put it back?",
+	"Did you lie??"
+];
 
 function theCallbackFunction (theError, theAnswer) {
+	console.log("");
 	console.log("Answer recorded");
 	console.log(`Answer: ${theAnswer}`);
 
-	options.prompt = "Do you find yourself initiating conversations?";
-	read(options, callbackNumberTwo);
-
-	function callbackNumberTwo (theErrorTwo, theAnswerTwo) {
-		console.log("Answer recorded");
-		console.log(`Answer: ${theAnswerTwo}`);		
+	i += 1;
 	
-		options.prompt = "Do you feel superior to others?";
-		read(options, callbackNumberThree);
-
-		function callbackNumberThree (theErrorThree, theAnswerThree) {
-			console.log("Answer recorded");
-			console.log(`Answer: ${theAnswerThree}`);	
-		
-			options.prompt = "Do you clean up after yourself?";
-			read(options, callbackNumberFour);
-
-			function callbackNumberFour (theErrorFour, theAnswerFour) {
-			console.log("Answer recorded");
-			console.log(`Answer: ${theAnswerFour}`);	
-			}
-		}
+	if (i < questionStrings.length) {
+		var options = { prompt: `Question #${i+1}: ${questionStrings[i]}`}
+		console.log("");
+		read(options, theCallbackFunction);	
 	}
 }
 
+var i = 0;
+
+var options = { prompt: `Question #${i+1}: ${questionStrings[i]}`}
+
 read(options, theCallbackFunction);
 
-console.log("");
-console.log("Last line");
+
+
+// function theCallbackFunction (theError, theAnswer) {
+// 	console.log("Answer recorded");
+// 	console.log(`Answer: ${theAnswer}`);
+
+// 	options.prompt = "Do you find yourself initiating conversations?";
+// 	read(options, callbackNumberTwo);
+
+// 	function callbackNumberTwo (theErrorTwo, theAnswerTwo) {
+// 		console.log("Answer recorded");
+// 		console.log(`Answer: ${theAnswerTwo}`);		
+	
+// 		options.prompt = "Do you feel superior to others?";
+// 		read(options, callbackNumberThree);
+
+// 		function callbackNumberThree (theErrorThree, theAnswerThree) {
+// 			console.log("Answer recorded");
+// 			console.log(`Answer: ${theAnswerThree}`);	
+		
+// 			options.prompt = "Do you clean up after yourself?";
+// 			read(options, callbackNumberFour);
+
+// 			function callbackNumberFour (theErrorFour, theAnswerFour) {
+// 			console.log("Answer recorded");
+// 			console.log(`Answer: ${theAnswerFour}`);	
+// 			}
+// 		}
+// 	}
+// }
+
+// read(options, theCallbackFunction);
+
+// console.log("");
+// console.log("Last line");
