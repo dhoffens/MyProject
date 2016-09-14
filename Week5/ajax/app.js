547fc2e8a27e5f8821ade275fe602cec3144e01b
Daniel Hoffens <dhoffens@gmail.com>
2016-09-14 10:46:23 -0400

    starting with ajax exercises

---
+++ Week5/ajax/app.js
@@ -0,0 +1,11 @@
+console.log("We are go!");
+
+$(document).ready(function () {
+	$('.js-fetch-characters').on('click', fetchCharacters);
+});
+
+function fetchCharacters () {
+	console.log('Fetch characters click');
+
+	//AJAX
+}
